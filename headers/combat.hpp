d1a920067e2b7b7f112e33353bf927171c9c60d9
Gr0sCrane <Mouadchd05@gmail.com>
2025-07-11 05:31:49 +0200

    working on combat

--- headers/combat.hpp
+++ headers/combat.hpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "board.hpp"
+#include "input.h"
 
 enum class GameState {
     FREE, // the player is free to move
     COMBAT, // Combat mode
     TITLE // Titlescreen
     //TODO maybe a pause mode
+};
+
+enum class Turn{
+    PLAYER,
+    MOB
 };
 
 void StartFight(Board& board,std::shared_ptr<Player> player, std::shared_ptr<Mob> mob);
--- src/combat.cpp
+++ src/combat.cpp
@@ -3,7 +3,40 @@
 
 void StartFight(Board& board,std::shared_ptr<Player> player, std::shared_ptr<Mob> mob){
 
-    bool isPlayerAttacked = false;
+    bool isCombatOver = false;
+    Turn currentTurn = Turn::PLAYER;
 
+    while (!isCombatOver) {
+        if (player->getStats().getHp() <= 0 || mob->getStats().getHp() <= 0) {
+            isCombatOver = true;
+            break;
+        }
 
+        if (currentTurn == Turn::PLAYER) {
+            // Afficher options : attaque (A), défense (P)
+            if (is_key_pressed(SDL_SCANCODE_A)) {
+                player->attack(mob);
+                player->setPlayerProtecting(false);
+                currentTurn = Turn::MOB;
+                SDL_Delay(300);
+            } 
+            else if (is_key_pressed(SDL_SCANCODE_P)) {
+                player->setPlayerProtecting(true);
+                currentTurn = Turn::MOB;
+                SDL_Delay(300);
+            }
+        } 
+        else if (currentTurn == Turn::MOB) {
+            if (player->isPlayerProtecting()) {
+                player->protect(mob->getStats().getAttack());
+            } else {
+                mob->attackPlayer(player);
+            }
+
+            currentTurn = Turn::PLAYER;
+            SDL_Delay(300);
+        }
+
+        //TODO interface graphique avec pv ect..
+    }
 }

efda4280b9573fa7ae89e3e56bd1607944a37831
Gr0sCrane <Mouadchd05@gmail.com>
2025-07-19 18:32:57 +0200

    update main.hpp because problem

--- headers/main.hpp
+++ headers/main.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 #include <vector>
 #include <SDL_ttf.h>
@@ -7,13 +6,6 @@
 #include "game_function.hpp"
 #include "entity_spawn.hpp"
 #include "combat.hpp"
-#include "algorithms.hpp"
-#include "player.hpp"
-#include "mob.hpp"
-#include "board.hpp"
-#include "entity.hpp"
-#include "gameStates.hpp"
-#include "stats.hpp"
 
 
 const Position kDefault_pos = Position(0,0);
